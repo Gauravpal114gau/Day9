1968a6164a6c06dc4cacd216e8c2320692217be7
Gaurav Pal <g1aurav.org114gmail.com>
2023-06-09 15:25:38 +0530

    b3-internal-module

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { } from "node: fs";
+import { readFileSync, writeFileSync } from "node: fs";
 
-let filePath = "C:\Users\g1aur\OneDrive\Desktop\MArch2023 HTML\Day9\package.json";
-let fileData = readFilesync(filePath, { encoding: "utf-8" });
-console.log(fileData);
- 
+let filePath = "C:\Users\g1aur\OneDrive\Desktop\MArch2023 HTML\Day9\src\output.txt";
+
+let data = `I am a good boy I am from kanpur`;
+
+writeFileSync(filePath, data);
+console.log("write success");
+
+
