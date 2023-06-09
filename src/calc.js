59981e096b3af4c79d1fc45a04b8fd5f01bdf221
Gaurav Pal <g1aurav.org114gmail.com>
2023-06-09 14:55:41 +0530

    b1-module-continue

--- src/calc.js
+++ src/calc.js
@@ -1,5 +1,8 @@
-function sum(n1, n2) {
+ export function sum(n1, n2) {
     return n1 + n2;
 }
+ export function sub(n1, n2) {
+    return n1 - n2;
+}
 
-export { sum };
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import { sum } from "./calc.js";
+import { sum , sub } from "./calc.js";
 
 console.log("Hello world");
 
 let output = sum(10, 20);
 console.log("sum= " + output);
 
+let output1 = sub(50, 10);
+console.log("Diff= " + output1);
 
  
