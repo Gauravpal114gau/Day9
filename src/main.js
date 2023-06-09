dedc75c014b2a0f4ce307339992da725976179ed
Gaurav Pal <g1aurav.org114gmail.com>
2023-06-09 15:52:09 +0530

    b7-module

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-  
+import { readFile } from "node:/promises";
+
+ filePath = "C:\Users\g1aur\OneDrive\Desktop\MArch2023 HTML\Day9\package.json";
+ async function main() {
+    let filePath = "C:\Users\g1aur\OneDrive\Desktop\MArch2023 HTML\Day9\package.json";
+
+    let fileData = await readFile( filePath , {encoding: "utf-8"});
+     console.log(fileData);
+}
+    
+main();
 
 
 
