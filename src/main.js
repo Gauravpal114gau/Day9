6ab7a2b0594c7be46a75b6548a8ca9bd816bfd97
Gaurav Pal <g1aurav.org114gmail.com>
2023-06-09 15:33:47 +0530

    b4-module

--- src/main.js
+++ src/main.js
@@ -1,10 +1,2 @@
-import { readFileSync, writeFileSync } from "node: fs";
-
-let filePath = "C:\Users\g1aur\OneDrive\Desktop\MArch2023 HTML\Day9\src\output.txt";
-
-let data = `I am a good boy I am from kanpur`;
-
-writeFileSync(filePath, data);
-console.log("write success");
-
+ 
 
