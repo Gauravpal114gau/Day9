ca6e13364fe055c077f0fb2bf19c47435f9b25fa
Gaurav Pal <g1aurav.org114gmail.com>
2023-06-09 15:33:58 +0530

    b4-module

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
- 
+console.log(" Hello1");
+console.log(" Hello2");
+
+setTimeout(() => console.log("Hello2.5"), 1000);
+console.log(" Hello3");
+console.log(" Hello4");
+console.log(" Hello5");
+
+
+
+
+
+
 
